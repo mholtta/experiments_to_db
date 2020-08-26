b72520a81c7fc8cab8e890c63ae727eeeac20d71
Matti Hölttä <matti2holtta@gmail.com>
2020-08-26 12:54:32 +0300

    Adding functionality for loading in test statistics

--- experiments_to_db.py
+++ experiments_to_db.py
@@ -5,6 +5,44 @@ import warnings
 from collections import defaultdict
 import json
 
+
+# Helper function for removing list wrapping for a single item that might be wrapped into a list
+def list_wrap_remove(var):
+    if type(var) != list:    
+        # If not a list, return the input
+        return var
+    elif len(var) > 1:
+        # If an actual list, return the list
+        return var
+    elif len(var) == 1:
+        return var[0]
+    else:
+        return var
+
+def metrics_file_processing(folder, metrics_file):
+    # Reading in metrics
+    metrics_raw = metrics_file.read()
+    metrics = json.loads(metrics_raw)
+
+    # Removing list wrapping from single items
+    metrics = { key: list_wrap_remove(value) for key, value in metrics.items()}
+
+    # Converting to defaultdict for easy default value
+    metrics = defaultdict(lambda: None, metrics)
+
+    test_statistics = get_test_statistics(folder ,metrics)
+
+    return test_statistics
+
+def get_test_statistics(folder, metrics_dict):
+
+
+    return [folder, metrics_dict["acc_macro_te"], metrics_dict["prec_macro_te"], metrics_dict["rec_macro_te"], 
+    metrics_dict["f1_macro_te"]]
+
+
+
+
 # Folders containing experiments 
 folders = glob.glob(folder_experiments)
 # Connecting to db and creating cursor
@@ -31,12 +69,16 @@ for folder in folders:
             print(name_space)
             # From list to dictionary with hyperparameter names as keys
             experiment_description = {item.split('=')[0] : item.split('=')[1] for item in name_space}
-            # COnverting to defaultdict for easy default value
+            # Converting to defaultdict for easy default value
             experiment_description = defaultdict(lambda: None, experiment_description)
 
             # Reading in metrics
             metrics_raw = m.read()
             metrics = json.loads(metrics_raw)
+            # Converting to defaultdict for easy default value
+            metrics = defaultdict(lambda: None, metrics)
+
+            test_statistics = [folder, list_wrap_remove(metrics)]
 
             # TODO need to get the best model by looking at which creteria used and then look at metrics, which has the largest value
             # TODO need to parse date and time from folder name
