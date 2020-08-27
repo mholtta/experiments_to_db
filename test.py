64534fd1b72c4078d7b6baeaf8599c873df139e4
Matti Hölttä <matti2holtta@gmail.com>
2020-08-27 19:04:33 +0300

    Added test cases and start for test file

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from file_processing import out_file_processing, metrics_file_processing
+
+class TestFileProcessing(unittest.TestCase):
+
+    def test_out_file_processing1(self):
+        
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
