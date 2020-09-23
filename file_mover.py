6d3903a8afe5cf90cd17844bdf37a236405ea7b4
Matti Hölttä <matti2holtta@gmail.com>
2020-09-23 15:45:11 +0300

    Moved file mover to this repo

---
+++ file_mover.py
@@ -0,0 +1,29 @@
+import glob
+import re
+import shutil
+from config import error_file_path, out_file_path, directories_path
+
+err_files = glob.glob(error_file_path)
+out_files = glob.glob(out_file_path)
+
+combined = err_files + out_files
+
+directories = glob.glob(directories_path)
+
+# Matches "-" followed by alphanumeric
+pattern = re.compile("(-)[0-9]+")
+
+for file in combined:
+    match = pattern.search(file)
+    job_id = match.group()[1:] # Removing "-" from beginning
+    filename = file.split('\\')[-1]
+    
+    # Looping through folders to find match
+    # Inefficient, but doesn't matter since not too many folders
+    for folder in directories:
+        if job_id in folder:
+            # print(job_id, folder,"\n")
+            new_path = "".join([folder,filename])
+            # print("new_path", new_path, "\n")
+            shutil.move(file, new_path)
+
